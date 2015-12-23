a5cdb4627daed37c89f3d086ef20745c27f7bd40
Binalfew Kassa <binalfew@yahoo.com>
2015-12-23 15:55:17 +0300

    Create default auth module

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace FlairBooks\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use FlairBooks\Http\Requests;
+use FlairBooks\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        return view('home');
+    }
+}
